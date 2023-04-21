88cf0661d87b38317ada68607c1f347a863a6f76
Gautamkumarsolanki <42197609+Gautamkumarsolanki@users.noreply.github.com>
2023-04-21 23:51:43 +0530

    added about

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment} from "react";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import Alert from "./component/Alert";
+import About from "./component/About"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import User from "./component/User";
 import GithubState from "./GithubState";
@@ -20,6 +21,7 @@ function App() {
             <Route path="/" element={<Home/>} errorElement={<Error/>}>
               <Route path="users/:name" element={<Home/>}/>
             </Route>
+            <Route path="about" element={<About/>}/>
             <Route path="user/:loginId" element={<User />} />
             <Route path='*' element={<Error/>}/>
           </Routes>
--- src/component/About.js
+++ src/component/About.js
@@ -1,14 +1,10 @@
 import React, { Fragment } from 'react'
-import { Link, Outlet } from 'react-router-dom'
 
 export default function About() {
   return (
     <Fragment>
         <h1>About</h1>
-        <Link to="display">Display|</Link>
-        <Link to="show">Show|</Link>
-        <Link to="nice">nice</Link>
-        <Outlet/>
+        
     </Fragment>
   )
 }
